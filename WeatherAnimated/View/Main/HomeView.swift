bcd4cd37e6e268dc8abbbf52ed6b1948a2d78a20
Lodkahs <omalojiu@gmail.com>
2023-04-17 17:44:40 +0300

    color extension

---
+++ WeatherAnimated/View/Main/HomeView.swift
@@ -0,0 +1,23 @@
+//
+//  HomeView.swift
+//  WeatherAnimated
+//
+//  Created by Andrew  on 4/17/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        ZStack {
+            Color.background
+                .ignoresSafeArea()
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
