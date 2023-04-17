ac0a328d079d5a3feb643ce8211336b94ce6b987
Lodkahs <omalojiu@gmail.com>
2023-04-17 17:28:02 +0300

    Initial Commit

---
+++ WeatherAnimated/WeatherAnimatedApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAnimatedApp.swift
+//  WeatherAnimated
+//
+//  Created by Andrew  on 4/17/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAnimatedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
