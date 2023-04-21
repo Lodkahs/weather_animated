d19fc4f29700ac8514052fd9727dd9f4128237f9
Lodkahs <omalojiu@gmail.com>
2023-04-21 21:39:21 +0300

    inner shadow

--- WeatherAnimated/View/Detail/ForecastView.swift
+++ WeatherAnimated/View/Detail/ForecastView.swift
@@ -14,6 +14,7 @@ struct ForecastView: View {
         }
         .backgroundBlur(radius: 25, opaque: true)
         .clipShape(RoundedRectangle(cornerRadius: 44))
+        .innerShadow(shape: RoundedRectangle(cornerRadius: 44), color: Color.bottomSheetBorderMiddle, lineWidth: 1, offsetX: 0, offsetY: 1, blur: 0, blenMode: .overlay, opacity: 1)
         .overlay {
             //MARK: - bottom sheep separator
             Divider()
